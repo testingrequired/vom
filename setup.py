7dd69ef6b61432dae686c2d638f7c5c7654e69cf
Kylee Tilley <kyleetilley@gmail.com>
2017-10-01 15:36:10 -0500

    Adding setup files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vom",
+    version="0.1.0",
+    description="An opinionated framework for writing page objects for selenium tests/scripts",
+    author="Kylee Tilley",
+    author_email="kyleetilley@gmail.com",
+    url="https://github.com/testingrequired/vom",
+    keywords=["selenium", "page-object", "testing", "automation", "test", "tests"],
+    license="MIT",
+    packages=find_packages(exclude=['venv']),
+    install_requires=['selenium'],
+    python_requires="~=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
