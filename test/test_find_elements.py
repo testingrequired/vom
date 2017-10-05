430e25604d5ea3b5f34327b53137c29ef40bd1bf
Kylee Tilley <kyleetilley@gmail.com>
2017-10-05 08:12:20 -0500

    Adding ability to define custom view classes to wrap elements in

---
+++ test/test_find_elements.py
@@ -0,0 +1,31 @@
+import pytest
+from vom import View
+from unittest.mock import Mock
+from selenium.webdriver.remote.webdriver import WebElement
+
+
+@pytest.fixture
+def element():
+    e = Mock(spec=WebElement)
+    return e
+
+@pytest.mark.skip("WIP")
+def test__custom_view_class__subclass__doesnt_raise_error(element):
+    view = View(lambda: element)
+    subclass = Mock(spec=View)
+
+    try:
+        view.find_elements("", "", subclass)
+    except ValueError:
+        pytest.fail(f"Error thrown with a subclass of View")
+
+
+def test__custom_view_class__non_subclass__raises_error(element):
+    view = View(lambda: element)
+
+    try:
+        view.find_elements("", "", Mock())
+    except ValueError:
+        pass
+    else:
+        pytest.fail(f"Error not thrown with a non subclass of View")
--- test/test_view.py
+++ test/test_view.py
@@ -1,8 +1,16 @@
 import pytest
 from vom import View
+from unittest.mock import Mock
+from selenium.webdriver.remote.webdriver import WebElement
 
-def test__view():
+
+@pytest.fixture
+def element():
+    return Mock(spec=WebElement)
+
+
+def test__view(element: WebElement):
     try:
-        View(lambda: None)
+        View(lambda: element)
     except Exception as e:
         pytest.fail(f"Failed to initialize View: {e}")
