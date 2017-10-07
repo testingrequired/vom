ef2c32f5fca7a501018a00ca38e4e556244b8a99
Kylee Tilley <kyleetilley@gmail.com>
2017-10-07 10:45:51 -0500

    Removing built docs

--- docs/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["vom"],titleterms:{document:[],indic:0,tabl:0,vom:0,welcom:[]}})
