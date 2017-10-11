96f43a6a5fd978f17070821883ce5c83b82e64b8
Kylee Tilley <kyleetilley@gmail.com>
2017-10-11 10:09:28 -0500

    Update test_find_element.py

--- test/test_find_element.py
+++ test/test_find_element.py
@@ -1,7 +1,1 @@
-import pytest
-from unittest.mock import Mock
 
-
-def test__custom_view_class__element_wrapped(view, custom_view_cls):
-    element = view.find_element("", "", view_cls=custom_view_cls)
-    assert isinstance(element, custom_view_cls.__class__), "Element not wrapped custom view class"
