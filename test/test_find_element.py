210d1ae9e97efa88c6b3fdfb1a6ecb062c1f40e5
Kylee Tilley <kyleetilley@gmail.com>
2017-10-11 10:05:00 -0500

    Removing custom view class tests

--- test/test_find_element.py
+++ test/test_find_element.py
@@ -5,19 +5,3 @@ from unittest.mock import Mock
 def test__custom_view_class__element_wrapped(view, custom_view_cls):
     element = view.find_element("", "", view_cls=custom_view_cls)
     assert isinstance(element, custom_view_cls.__class__), "Element not wrapped custom view class"
-
-
-def test__custom_view_class__subclass__doesnt_raise_error(view, custom_view_cls):
-    try:
-        view.find_element("", "", view_cls=custom_view_cls)
-    except ValueError:
-        pytest.fail(f"Error thrown with a subclass of View")
-
-
-def test__custom_view_class__non_subclass__raises_error(view):
-    try:
-        view.find_element("", "", view_cls=Mock())
-    except ValueError:
-        pass
-    else:
-        pytest.fail(f"Error not thrown with a non subclass of View")
