83e3e6d0b379681757a9c9ab898b16c55f80822e
Kylee Tilley <kyleetilley@gmail.com>
2017-10-11 10:05:24 -0500

    Removing custom view class tests from find_elements

--- test/test_find_elements.py
+++ test/test_find_elements.py
@@ -13,19 +13,3 @@ def test__find_elements_by_text__custom_selector(view):
 def test__custom_view_class__elements_wrapped(view, custom_view_cls):
     elements = view.find_elements("", "", view_cls=custom_view_cls)
     assert all([isinstance(e, custom_view_cls.__class__) for e in elements]), "Elements not wrapped custom view class"
-
-
-def test__custom_view_class__subclass__doesnt_raise_error(view, custom_view_cls):
-    try:
-        view.find_elements("", "", custom_view_cls)
-    except ValueError:
-        pytest.fail(f"Error thrown with a subclass of View")
-
-
-def test__custom_view_class__non_subclass__raises_error(view):
-    try:
-        view.find_elements("", "", Mock())
-    except ValueError:
-        pass
-    else:
-        pytest.fail(f"Error not thrown with a non subclass of View")
