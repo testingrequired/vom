eb70fe28bffd136004adf9aba40f8457aa93aa4e
Kylee Tilley <kyleetilley@gmail.com>
2017-12-18 22:58:15 -0600

    Refactoring for 2.7 compatibility

--- vom/__init__.py
+++ vom/__init__.py
@@ -1,7 +1,8 @@
-from typing import Union, Callable, List
+from typing import Union, Callable, List, Any
 from selenium.webdriver.remote.webdriver import WebDriver, WebElement, By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.common.exceptions import NoSuchElementException
+from future.utils import raise_from
 
 
 class View(object):
@@ -9,7 +10,8 @@ class View(object):
     Base View
     """
 
-    def __init__(self, target: Union[WebDriver, Callable[[], WebElement]], parent: "View" = None):
+    def __init__(self, target, parent=None):
+        # type: (target: Union[WebDriver, Callable[[], WebElement]], View)
         """
         View accepts either a WebDriver instance or a callable returning a WebElement
 
@@ -24,11 +26,9 @@ class View(object):
             # or
 
             view = View(get_example_form)
-
-        :param Union[WebDriver, Callable[[], WebElement] target:
         """
-        self._root: Union[Callable[[], WebElement], Callable[[], None]] = lambda: None
-        self._driver: Union[Callable[[], WebDriver], None] = None
+        self._root = lambda: None
+        self._driver = None
         self.parent = parent
 
         if isinstance(target, WebDriver):
@@ -41,35 +41,42 @@ class View(object):
         return self.text
 
     @property
-    def root(self) -> WebElement:
+    def root(self):
+        # type: () -> WebElement
         return self._root()
 
     @root.setter
     def root(self, value):
+        # type: () -> WebElement
         self._root = value
 
     @property
     def driver(self):
+        # type: () -> WebDriver
         try:
             d = self._driver()
         except NoSuchElementException as e:
-            raise RuntimeError(f"Root element not present on the page") from e
+            raise_from(RuntimeError("Root element not present on the page"), e)
         else:
             return d
 
     @driver.setter
     def driver(self, value):
+        # type: (WebDriver) -> None
         self._driver = value
 
-    def _init_from_driver(self, driver: WebDriver):
-        self.driver: WebDriver = lambda: driver
+    def _init_from_driver(self, driver):
+        # type: (WebDriver) -> None
+        self.driver = lambda: driver
         self.root = lambda: self.driver.find_element_by_tag_name("html")
 
-    def _init_from_callable(self, root: Callable[[], WebElement]):
+    def _init_from_callable(self, root):
+        # type: (Callable[[], WebElement]) -> None
         self.root = root
-        self.driver: Callable[[], WebDriver] = lambda: self.root.parent
+        self.driver = lambda: self.root.parent
 
-    def __eq__(self, other: "View"):
+    def __eq__(self, other):
+        # type: (View) -> bool
         return self.id == other.id
 
     @property
@@ -79,44 +86,53 @@ class View(object):
     # Properties
 
     def get_attribute(self, name):
+        # type: (str) -> None
         return self.root.get_attribute(name)
 
     def get_property(self, name):
+        # type: (str) -> Any
         return self.root.get_property(name)
 
     @property
     def title(self):
+        # type: () -> str
         return self.execute_script("return arguments[0].title")
 
     @property
     def tag_name(self):
+        # type: () -> str
         return self.root.tag_name
 
     @property
     def text(self):
+        # type: () -> str
         return self.root.text
 
     # Transform
 
     @property
     def as_select(self):
+        # type: () -> Select
         return Select(self.root)
 
-    ## Content
+    # Content
 
     @property
     def inner_html(self):
+        # type: () -> str
         return self.root.get_attribute("innerHTML")
 
     @property
     def outer_html(self):
+        # type: () -> str
         return self.root.get_attribute("outerHTML")
 
     @property
     def inner_text(self):
+        # type: () -> str
         return self.root.get_attribute("innerText")
 
-    ## Dimensions/Location
+    # Dimensions/Location
 
     @property
     def rect(self):
@@ -134,18 +150,21 @@ class View(object):
     def location_once_scrolled_into_view(self):
         return self.root.location_once_scrolled_into_view
 
-    ## State
+    # State
 
     @property
     def is_enabled(self):
+        # type: () -> bool
         return self.root.is_enabled()
 
     @property
     def is_selected(self):
+        # type: () -> bool
         return self.root.is_selected()
 
     @property
     def is_displayed(self):
+        # type: () -> bool
         result = False
         try:
             result = self.root.is_displayed()
@@ -155,20 +174,24 @@ class View(object):
             return result
 
     def has_class(self, value):
+        # type: (str) -> bool
         classes = self.get_attribute("class").split(" ")
         return value in classes
 
-    ## Style/CSS
+    # Style/CSS
 
     def value_of_css_property(self, property_name):
+        # type: (str) -> str
         return self.root.value_of_css_property(property_name)
 
     # Javascript
 
     def execute_script(self, script, *args):
-        return self.driver.execute_script(script, *[self.root, *args])
+        # type: (str, *Any) -> Any
+        return self.driver.execute_script(script, *([self.root] + args))
 
     def execute_async_script(self, script, *args):
+        # type: (str, *Any) -> Any
         return self.driver.execute_async_script(script, *args)
 
     # Actions
@@ -179,23 +202,28 @@ class View(object):
     def clear(self):
         self.root.clear()
 
-    def send_keys(self, value, clear: bool = False):
+    def send_keys(self, value, clear=False):
+        # type: (str, bool) -> None
         if clear:
             self.clear()
         self.root.send_keys(value)
 
     def submit(self):
+        # type: () -> None
         self.root.submit()
 
     def focus(self):
+        # type: () -> None
         self.execute_script("arguments[0].focus()")
 
     def blur(self):
+        # type: () -> None
         self.execute_script("arguments[1].blur()")
 
     # Find Element/s
 
-    def find_element(self, by: By, value, view_cls: "View" = None) -> "View":
+    def find_element(self, by, value, view_cls=None):
+        # type: (By, Any, View) -> View
         """
         Find one element matching condition
         :param by: Type of condition
@@ -208,7 +236,8 @@ class View(object):
 
         return view_cls(lambda: self.root.find_element(by, value), self)
 
-    def find_elements(self, by: By, value, view_cls: "View" = None) -> List["View"]:
+    def find_elements(self, by, value, view_cls=None):
+        # type: (By, Any, Value) -> List[View]
         """
         Find one or more elements matching condition
         :param by: Type of condition
@@ -234,83 +263,109 @@ class View(object):
 
         return [view_cls(get_element_at_index(i), self) for i, element in enumerate(get_elements())]
 
-    def find_element_by_css_selector(self, value, view_cls = None):
+    def find_element_by_css_selector(self, value, view_cls=None):
+        # type: (str, View) -> View
         return self.find_element(By.CSS_SELECTOR, value, view_cls)
 
-    def find_elements_by_css_selector(self, value, view_cls = None):
+    def find_elements_by_css_selector(self, value, view_cls=None):
+        # type: (str, View) -> List[View]
         return self.find_elements(By.CSS_SELECTOR, value, view_cls)
 
-    def find_element_by_tag_name(self, value, view_cls = None):
+    def find_element_by_tag_name(self, value, view_cls=None):
+        # type: (str, View) -> View
         return self.find_element(By.TAG_NAME, value, view_cls)
 
-    def find_elements_by_tag_name(self, value, view_cls = None):
+    def find_elements_by_tag_name(self, value, view_cls=None):
+        # type: (str, View) -> List[View]
         return self.find_elements(By.TAG_NAME, value, view_cls)
 
-    def find_element_by_xpath(self, value, view_cls = None):
+    def find_element_by_xpath(self, value, view_cls=None):
+        # type: (str, View) -> View
         return self.find_element(By.XPATH, value, view_cls)
 
-    def find_elements_by_xpath(self, value, view_cls = None):
+    def find_elements_by_xpath(self, value, view_cls=None):
+        # type: (str, View) -> List[View]
         return self.find_elements(By.XPATH, value, view_cls)
 
-    def find_element_by_class_name(self, value, view_cls = None):
+    def find_element_by_class_name(self, value, view_cls=None):
+        # type: (str, View) -> View
         return self.find_element(By.CLASS_NAME, value, view_cls)
 
-    def find_elements_by_class_name(self, value, view_cls = None):
+    def find_elements_by_class_name(self, value, view_cls=None):
+        # type: (str, View) -> List[View]
         return self.find_elements(By.CLASS_NAME, value, view_cls)
 
-    def find_element_by_id(self, value, view_cls = None):
+    def find_element_by_id(self, value, view_cls=None):
+        # type: (str, View) -> View
         return self.find_element(By.ID, value, view_cls)
 
-    def find_elements_by_id(self, value, view_cls = None):
+    def find_elements_by_id(self, value, view_cls=None):
+        # type: (str, View) -> List[View]
         return self.find_elements(By.ID, value, view_cls)
 
-    def find_element_by_link_text(self, value, view_cls = None):
+    def find_element_by_link_text(self, value, view_cls=None):
+        # type: (str, View) -> View
         return self.find_element(By.LINK_TEXT, value, view_cls)
 
-    def find_elements_by_link_text(self, value, view_cls = None):
+    def find_elements_by_link_text(self, value, view_cls=None):
+        # type: (str, View) -> List[View]
         return self.find_elements(By.LINK_TEXT, value, view_cls)
 
-    def find_element_by_partial_link_text(self, value, view_cls = None):
+    def find_element_by_partial_link_text(self, value, view_cls=None):
+        # type: (str, View) -> View
         return self.find_element(By.PARTIAL_LINK_TEXT, value, view_cls)
 
-    def find_elements_by_partial_link_text(self, value, view_cls = None):
+    def find_elements_by_partial_link_text(self, value, view_cls=None):
+        # type: (str, View) -> List[View]
         return self.find_elements(By.PARTIAL_LINK_TEXT, value, view_cls)
 
-    def find_element_by_name(self, value, view_cls = None):
+    def find_element_by_name(self, value, view_cls=None):
+        # type: (str, View) -> View
         return self.find_element(By.NAME, value, view_cls)
 
-    def find_elements_by_name(self, value, view_cls = None):
+    def find_elements_by_name(self, value, view_cls=None):
+        # type: (str, View) -> List[View]
         return self.find_elements(By.NAME, value, view_cls)
 
-    def find_elements_by_text(self, value, selector="*", view_cls = None):
+    def find_elements_by_text(self, value, selector="*", view_cls=None):
+        # type: (str, str, View) -> List[View]
         return [e for e in self.find_elements_by_css_selector(selector, view_cls) if e.text == value]
 
-    def find_element_by_text(self, value, selector="*", view_cls = None):
+    def find_element_by_text(self, value, selector="*", view_cls=None):
+        # type: (str, str, View) -> View
         elements = self.find_elements_by_text(value, selector, view_cls)
         if len(elements) == 0:
-            raise NoSuchElementException(f"No elements found with text: `{value}`")
+            raise NoSuchElementException(
+                "No elements found with text: `{}`".format(value))
         return elements[0]
 
-    def find_elements_by_partial_text(self, value, selector="*", view_cls = None):
+    def find_elements_by_partial_text(self, value, selector="*", view_cls=None):
+        # type: (str, str, View) -> List[View]
         return [e for e in self.find_elements_by_css_selector(selector, view_cls) if value in e.text]
 
-    def find_element_by_partial_text(self, value, selector="*", view_cls = None):
-        elements = self.find_elements_by_partial_text(value, selector, view_cls)
+    def find_element_by_partial_text(self, value, selector="*", view_cls=None):
+        # type: (str, str, View) -> View
+        elements = self.find_elements_by_partial_text(
+            value, selector, view_cls)
         if len(elements) == 0:
-            raise NoSuchElementException(f"No elements found with text: `{value}`")
+            raise NoSuchElementException(
+                "No elements found with text: `{}`".format(value))
         return elements[0]
 
     def find_inputs_by_placeholder(self, value, view_cls=None):
-        return self.find_elements_by_css_selector(f"input[type=text][placeholder='{value}']", view_cls)
+        # type: (str, str, View) -> List[View]
+        return self.find_elements_by_css_selector("input[type=text][placeholder='{}']".format(value), view_cls)
 
     def find_input_by_placeholder(self, value, view_cls=None):
-        return self.find_element_by_css_selector(f"input[placeholder='{value}']", view_cls)
-
+        # type: (str, str, View) -> View
+        return self.find_element_by_css_selector("input[placeholder='{}']".format(value), view_cls)
 
     # Waiting
 
     def wait_until_displayed(self, timeout=10):
+        # type: (int) -> None
         return WebDriverWait(None, timeout).until(lambda _: self.is_displayed)
 
     def wait_until_not_displayed(self, timeout=10):
+        # type: (int) -> None
         return WebDriverWait(None, timeout).until(lambda _: not self.is_displayed)
