99c7421a35d570732c55e9894b1e6c57a28a9811
Kylee Tilley <kyleetilley@gmail.com>
2017-12-19 21:31:53 -0600

    Refactoring

--- test/test_view.py
+++ test/test_view.py
@@ -1,12 +1,5 @@
 import pytest
 from vom import View
-from mock import Mock
-from selenium.webdriver.remote.webdriver import WebElement
-
-
-@pytest.fixture
-def element():
-    return Mock(spec=WebElement)
 
 
 def test__view(element):
